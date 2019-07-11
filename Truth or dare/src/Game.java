632fb8f72f33f9ec7e2558bc4a12594ec55de9d0
TheQ-Creater <52795065+TheQ-Creater@users.noreply.github.com>
2019-07-11 18:54:56 +0200

    Update Game.java

--- Truth or dare/src/Game.java
+++ Truth or dare/src/Game.java
@@ -7,3 +7,70 @@ public class Game {
 	}
 
 }
+
+package com.theq.einmalrichtig;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class Main4Activity extends Activity {
+
+    Button button9;
+    Button button10;
+    TextView Tv4;
+
+    Random W;
+
+    String[] Warheit = new String[]{"Hast du (heute) schon daran gedacht mit einem aus der Gruppe zu schlafen?",};
+
+    Random P;
+
+    String[] Pflicht = new  String[]{"Trinke eine Body shot von wem du willst. Weigert sich einer Trinkt ihr beide 4 Schlücke.",};
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+        button9 = findViewById(R.id.button9);
+        button10 = findViewById(R.id.button10);
+        Tv4 = findViewById(R.id.TV4);
+
+
+        W = new Random();
+        P = new Random();
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Tv4.setText(Warheit[W.nextInt(Warheit.length)]);
+
+
+                button10.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Tv4.setText(Pflicht[P.nextInt(Pflicht.length)]);
+                    }
+
+                });
+
+
+
+
+            }
+        });
+
+    }
+
+
+}
+
+
+
